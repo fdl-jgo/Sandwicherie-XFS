99af807b0c4d566c25f3e7ff493a8e63f6c7be03
NoxWorld258 <xavier.inghels@gmail.com>
2017-10-13 14:21:49 +0200

    Deplacement vues racines => vues comptes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,41 +7,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    // Base
     public function indexAction()
     {
         return $this->render('AppBundle:Default:index.html.twig');
     }
 
+    // @Fidel
     public function sandwichsAction()
     {
         return $this->render('AppBundle:Default:sandwichs.html.twig');
     }
 
+    // @Fidel
     public function contactAction()
     {
         return $this->render('AppBundle:Default:contact.html.twig');
     }
-
+    
+    // @Xavier
     public function loginAction()
     {
     	return $this->render('AppBundle::login_page.html.twig');
     }
 
+    // @Xavier
     public function inscriptionAction()
     {
     	return $this->render('AppBundle:Compte:connexion.html.twig');
     }
 
+    // @Xavier
     public function commandeAction()
     {
         return $this->render('AppBundle::commande.html.twig');
     }
 
+    // @Xavier
      public function historiqueAction()
     {
         return $this->render('AppBundle:historique.html.twig');
     }
 
+    // @Xavier
      public function panierAction()
     {
         return $this->render('AppBundle:panier.html.twig');
